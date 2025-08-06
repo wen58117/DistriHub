43bf14b0adf3d7095d4864cb862724c550b734e2
xuwenjie <wen_58117@163.com>
2025-08-06 11:22:25 +0800

    feat:项目初始化

---
+++ src/main/java/com/distrihub/DistriHubApplication.java
@@ -0,0 +1,13 @@
+package com.distrihub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistriHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistriHubApplication.class, args);
+    }
+
+}
