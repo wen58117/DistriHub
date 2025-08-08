8e5a6107f4390ff0f47a46757c2ff72631846d20
xuwenjie <wen_58117@163.com>
2025-08-08 11:13:52 +0800

    feat:nacos初始化测试

--- src/main/java/com/distrihub/DistriHubApplication.java
+++ src/main/java/com/distrihub/DistriHubApplication.java
@@ -2,8 +2,10 @@ package com.distrihub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class DistriHubApplication {
 
     public static void main(String[] args) {
